dbbd23fb4d3a815c20230fd0bc981db3ac9bb630
HanShin <vlrkwlepd@gmail.com>
2015-05-11 15:52:49 +0900

    Builder

---
+++ Builder/src/Builder.java
@@ -0,0 +1,9 @@
+/**
+ * Created by H2014154 on 2015-05-11.
+ */
+public abstract class Builder {
+    public abstract void makeTitle(String title);
+    public abstract void makeString(String str);
+    public abstract void makeItems(String[] items);
+    public abstract void close();
+}
---
+++ Builder/src/Director.java
@@ -0,0 +1,24 @@
+/**
+ * Created by H2014154 on 2015-05-11.
+ */
+public class Director {
+    private Builder builder;
+    public Director(Builder builder){
+        this.builder = builder;
+    }
+    public void construct(){
+        builder.makeTitle("Greeeting");
+        builder.makeString("아침과 낮에");
+        builder.makeItems(new String[]{
+                "좋은 아침입니다.",
+                "안녕하세요."
+        });
+        builder.makeString("밤에");
+        builder.makeItems(new String[]{
+                "안녕하세요.",
+                "안녕히 주무세요.",
+                "안녕히 계세요."
+        });
+        builder.close();
+    }
+}
