ef221de450d2809e6926ca1a6ce68080e3f89227
HanShin <vlrkwlepd@gmail.com>
2015-05-13 17:04:19 +0900

    Decorator

---
+++ Decorator/src/Border.java
@@ -0,0 +1,10 @@
+/**
+ * Created by H2014154 on 2015-05-13.
+ */
+public abstract class Border extends Display{
+    protected Display display;
+    protected Border(Display display){
+        this.display = display;
+    }
+
+}
