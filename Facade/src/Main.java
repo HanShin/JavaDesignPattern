4ae83232f2dcd0c11b98f69331e8356d6f907510
HanShin <vlrkwlepd@gmail.com>
2015-05-15 11:06:51 +0900

    Facade

---
+++ Facade/src/Main.java
@@ -0,0 +1,10 @@
+import pagemaker.PageMaker;
+
+/**
+ * Created by H2014154 on 2015-05-15.
+ */
+public class Main {
+    public static void main(String[] args){
+        PageMaker.makeWelcomePage("youngjin@youngjin.com","welcome.html");
+    }
+}
