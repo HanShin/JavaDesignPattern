aa522b57f3b414fb6a2bfcd2841335286aa5033e
HanShin <vlrkwlepd@gmail.com>
2015-06-17 15:38:29 +0900

    싱글톤 패턴 구현하는 가장 좋은 방법 : 하나의 요소를 갖는 열거형 타입으로 만든다.

--- Singleton/src/Main.java
+++ Singleton/src/Main.java
@@ -4,9 +4,13 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Start.");
-        Singleton obj1 = Singleton.getInstance();
+        SingletoneEnum obj1 = SingletoneEnum.INSTANCE;
+        SingletoneEnum obj2 = SingletoneEnum.INSTANCE;
+        obj1.TestMethod();
+        obj2.TestMethod();
+        /*Singleton obj1 = Singleton.getInstance();
         Singleton obj2 = Singleton.getInstance();
-
+        */
         if(obj1 == obj2){
             System.out.println("obj1과 obj2는 같은 인스턴스입니다.");
         } else{
---
+++ Singleton/src/SingletoneEnum.java
@@ -0,0 +1,9 @@
+/**
+ * Created by H2014154 on 2015-06-17.
+ */
+public enum SingletoneEnum {
+    INSTANCE;
+    public void TestMethod(){
+        System.out.println("½Ì±ÛÅæ ¿­°ÅÇü");
+    }
+}
